d1ad2b78eaac8d746876782a958c664691ac813e
lining05 <lining05@kuaishou.com>
2021-01-06 01:59:39 +0800

    name 修改

--- Example/Masonry_LayoutGuideExt/MASViewController.h
+++ Example/Masonry_LayoutGuideExt/MASViewController.h
@@ -2,8 +2,8 @@
 //  MASViewController.h
 //  Masonry_LayoutGuideExt
 //
-//  Created by lining05 on 12/27/2020.
-//  Copyright (c) 2020 lining05. All rights reserved.
+//  Created by inspiredtip on 12/27/2020.
+//  Copyright (c) 2020 inspiredtip. All rights reserved.
 //
 
 @import UIKit;
